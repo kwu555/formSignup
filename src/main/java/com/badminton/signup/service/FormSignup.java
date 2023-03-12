6eb0a19e5aa243c7e9b4c07149c45ef2a004d44b
haijiewu <kevinwu4@me.com>
2023-03-12 16:52:51 +0000

    added logic

---
+++ src/main/java/com/badminton/signup/service/FormSignup.java
@@ -0,0 +1,86 @@
+package com.badminton.signup.service;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FormSignup{
+
+    public static final String XPATH_EMAIL = "//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div[1]/input";
+    public static final String XPATH_FULL_TUES_TEXT = "//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div[1]/label/div/div[2]/div/span";
+    public static final String XPATH_NAME_CLICK_DROPDOWN = "//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div";
+    public static final String XPATH_NAME_SELECT_NAME = "//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[2]";
+    public static final String XPATH_REGISTER_TICK = "//*[@id=\"i25\"]";
+    public static final String XPATH_SUBMIT = "//*[@id=\"mG61Hd\"]/div[2]/div/div[3]/div/div[1]/div";
+
+    public static final String BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfA9zHG9OUYUCr57fC7CuAKyvf1_DmqdC_kZrBB4gGRNN48fQ/viewform";
+
+    public boolean signup(String chooseName){
+
+        WebDriver driver = chromeSetup();
+//        WebDriver driver = firefoxSetup();
+
+        boolean successfulSignup = false;
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(BASE_URL);
+
+        // get the actual value of the title
+        String isFullText = driver.findElement(By.xpath(XPATH_FULL_TUES_TEXT)).getText();
+
+        try{
+            // not full, then continue
+            if(isFullText.equals("*FULL on Tuesday*")){
+
+                // email
+                driver.findElement(By.xpath(XPATH_EMAIL)).sendKeys("kwu0521@gmail.com");
+                Thread.sleep(2000);
+
+                // name dropdown
+                driver.findElement(By.xpath(XPATH_NAME_CLICK_DROPDOWN)).findElements(By.xpath("div")).get(0).click();
+                Thread.sleep(2000);
+
+                List<WebElement> names = driver.findElement(By.xpath(XPATH_NAME_SELECT_NAME)).findElements(By.xpath("div"));
+                Optional<WebElement> name = names.stream().filter(x->x.getText().equals(chooseName)).findFirst();
+                name.ifPresent(WebElement::click);
+
+                Thread.sleep(2000);
+                // TICK Tuesday
+                driver.findElement(By.xpath(XPATH_REGISTER_TICK)).click();
+                Thread.sleep(2000);
+
+                // submit
+//			driver.findElement(By.xpath(XPATH_SUBMIT)).click();
+                Thread.sleep(2000);
+                successfulSignup = true;
+            }
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        driver.close();
+        return successfulSignup;
+    }
+
+    private WebDriver chromeSetup(){
+        System.setProperty("webdriver.chrome.driver","/Users/haijiewu/Desktop/Intellij/signup/src/main/resources/driver/chromedriver_mac64/chromedriver");
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+
+        return new ChromeDriver(ops);
+    }
+
+    private WebDriver firefoxSetup(){
+        System.setProperty("webdriver.gecko.driver","/Users/haijiewu/Desktop/Intellij/signup/src/main/resources/driver/chromedriver_mac64/chromedriver");
+
+        return new FirefoxDriver();
+    }
+}
